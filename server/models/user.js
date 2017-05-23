be14ba3ff4b8bb49086363f9b07aa8437534d5c7
Akhilesh Srivastava <akhil110@gmail.com>
2017-05-22 21:43:37 -0700

    Create user.js

---
+++ server/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    firstname: {type:String},
+    lastname: {type:String},
+    email: {type:String},
+    username: {type:String},
+	password: {type:String},
+    lastlogin: {type:Date}
+});
+
+// Pre-save of user's hash password to database
+UserSchema.pre('save', function (next) {
+  const users = this,
+    SALT_FACTOR = 5;
+
+  if (!users.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_FACTOR, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(users.password, salt, null, (err, hash) => {
+      if (err) return next(err);
+      users.password = hash;
+      next();
+    });
+  });
+});
+
+// Method to compare password for login
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) { return cb(err); }
+
+    cb(null, isMatch);
+  });
+};
+
+module.exports = mongoose.model('users', UserSchema, 'users');
+
