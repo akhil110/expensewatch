0c41a8018acf6a858c29d951426493bd8567b1ca
Akhilesh Srivastava <akhil110@gmail.com>
2017-05-22 22:24:21 -0700

    Add files via upload

---
+++ src/app/home/login.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ToastrService } from '../common/toastr.service'
+import { AuthService } from '../user/auth.service';
+
+@Component({
+  templateUrl: './login.component.html'
+})
+
+export class LoginComponent {
+
+  constructor(private fb: FormBuilder, 
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService) {
+  }
+
+  username = new FormControl('', [Validators.required]);
+  password = new FormControl('', [Validators.required]);
+
+  loginForm: FormGroup = this.fb.group({
+    username: this.username,
+    password: this.password,
+  });
+
+
+  loginUser(){
+    if (this.loginForm.dirty && this.loginForm.valid) {
+      this.authService.login(this.loginForm.value)
+        .subscribe(data => {
+          if (data.json().success === false) {
+            this.toastr.error(data.json().message);
+          } else {
+            this.toastr.success('Login successful.');
+            this.router.navigate(['report']);
+          }
+          this.loginForm.reset();
+        });
+    }
+  }
+  
+}
