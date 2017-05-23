ae4d9f8e01215ba904fb059d958bcef9df4e97ae
Akhilesh Srivastava <akhil110@gmail.com>
2017-05-22 22:05:21 -0700

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core'
+
+import { AuthService } from './user/auth.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+
+export class AppComponent {
+  constructor( private authService: AuthService, ) {
+    
+  }
+}
