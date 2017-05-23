f69cef5a1f20fb753c5f38622eb9a8f9692feb4f
Akhilesh Srivastava <akhil110@gmail.com>
2017-05-22 21:43:52 -0700

    Add files via upload

---
+++ server/models/expense.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+var mongoosePaginate = require('mongoose-paginate');
+
+const Schema = mongoose.Schema;
+
+const ExpenseSchema = new Schema({
+	userid: {type:String, required: true},
+    expensedate: {type:Date, required: true},
+    expensetype: {type:String, required: true},
+	expenseamt: {type:Number, required: true},
+    expensedesc: {type:String}
+});
+
+ExpenseSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model('expenses', ExpenseSchema, 'expenses');
